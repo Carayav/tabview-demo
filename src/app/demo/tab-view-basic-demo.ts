61045e2e3c1ed76cbd2e86551687fe171fda5dfe
Claudio Araya <claudio@idra.cl>
2023-08-18 11:49:22 -0400

    .

--- src/app/demo/tab-view-basic-demo.ts
+++ src/app/demo/tab-view-basic-demo.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
+import { TabPanel, TabView } from "primeng/tabview";
 
 @Component({
-    selector: 'tab-view-basic-demo',
-    templateUrl: './tab-view-basic-demo.html'
+  selector: "tab-view-basic-demo",
+  templateUrl: "./tab-view-basic-demo.html",
 })
-export class TabViewBasicDemo {}
+export class TabViewBasicDemo {
+  headerClass = "red";
+
+  CambiarClaseHeader(tabView: TabView) {
+    this.headerClass = this.headerClass === "red" ? "green" : "red";
+    setTimeout(() => {
+      tabView.cd.markForCheck();
+    }, 0);
+  }
+}
